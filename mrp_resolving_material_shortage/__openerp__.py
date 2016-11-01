f23100ef6dffb7ea6eafc7fedcf141ace3f54a7d
hanelsoft <dungta2@hanelsoft.vn>
2016-11-01 11:44:51 +0700

    fix error when open manufacturing settings

--- mrp_resolving_material_shortage/__openerp__.py
+++ mrp_resolving_material_shortage/__openerp__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Manufacturing - Stock Operations',
     'summary': 'Auto resolve shortage of Raw Materials by Internal Transfer or Procurement Order',
-    'version': '1.0',
+    'version': '1.0.1',
     'category': 'Manufacturing',
     'description': """
         Auto resolve shortage of Raw Materials by Internal Transfer or Procurement Order
@@ -9,11 +9,7 @@
     'author': "Hanel Software Solutions",
     'website': 'http://www.hanelsoft.vn/',
     'depends': ['mrp'],
-    'data': ['views/product_view.xml',
-             'views/mrp_view.xml',
-             'views/stock_view.xml',
-             'configs/mrp_config_data.xml'
-             ],
+    'data': ['views/mrp_view.xml', 'configs/mrp_config_data.xml'],
     'installable': True,
     'price': 80.00,
     'currency': 'EUR',
